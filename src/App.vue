c3c5bb9e49cedf951c09b6d868dcb42c003798bc
TkoolerLufar <TkoolerLufar@users.noreply.github.com>
2019-05-06 21:50:24 +0900

    アプリ名調整

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>例のあれがああなる確率</span>
-        <!--span class="font-weight-light">MATERIAL DESIGN</span-->
+        <span class="font-weight-light">あれがああなる確率</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
