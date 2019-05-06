f8e20a5dc286afec9a445fd2873376ce0e6367fc
TkoolerLufar <TkoolerLufar@users.noreply.github.com>
2019-05-06 21:50:32 +0900

    UI 調整

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -13,6 +13,11 @@
       <v-flex xs12>
         <v-text-field
           v-model="probabilityPerTry"
+          type="number"
+          min="0"
+          max="100"
+          step="0.5"
+          hint="成功率"
           suffix="%"
         />
       </v-flex>
@@ -26,6 +31,11 @@
       <v-flex xs12>
         <v-text-field
           v-model="numTries"
+          type="number"
+          min="1"
+          max="2147483647"
+          step="1"
+          hint="試行回数"
           suffix="回"
         />
       </v-flex>
@@ -39,6 +49,11 @@
       <v-flex xs12>
         <v-text-field
           v-model="numExpectedSuccesses"
+          type="number"
+          min="0"
+          max="2147483647"
+          step="1"
+          hint="期待する成功回数"
           suffix="回"
         />
       </v-flex>
@@ -59,6 +74,9 @@
 <script>
 /**
    * 累乗
+   *
+   * y がある程度大きい時、単純なループより速いです。
+   *
    * @returns {number} 冪
    * @param {number} x 仮数
    * @param {number} y 指数（自然数）
@@ -80,12 +98,24 @@
    * @returns {number} 勝率
    * @param {number} probabilityPerTry 単発の出現確率
    * @param {number} numTries 連発数（確定枠除く）
-   * @param {number} numExpectedSuccesses 目玉が欲しい数
+   * @param {number} numExpectedSuccesses 目玉が欲しい数（確定枠除く）
    */
   function calcGacha(probabilityPerTry, numTries, numExpectedSuccesses) {
+    /**
+     * @type {number} 所要時間のしきい値。ミリ秒。
+     * これ以内に計算できないと NaN を返します。
+     * (ハングアップ対策)
+     */
+    var CALC_TIME_THRESHOLD = 100;
+
     var p = 1;
     var combination = 1;
+    // 1秒以内に求まらなければ失敗
+    var t0 = new Date().getTime();
     for (var i = 0; i < numExpectedSuccesses; i++) {
+      if(new Date().getTime() - t0 >= CALC_TIME_THRESHOLD) {
+        return NaN;
+      }
       if (i) {
         combination = combination *
         (numTries-i+1) / i;
@@ -111,23 +141,24 @@
           p = Number(this.probabilityPerTry)/100,
           N = Number(this.numTries),
           n = Number(this.numExpectedSuccesses);
+        // p, N, n はそれぞれ順に実数、整数、整数
         if(!isFinite(p) || ![N, n].reduce(
           (result, x) => result && Number.isSafeInteger(x),
           true
         )) {
           return '????';
         }
-        if(p<0)return NaN;
-        if(p>1)return NaN;
-        if(N<1)return NaN;
-        if(N<n)return NaN;
-        if(n<0)return NaN;
-        if(n>9999)return '????';
-        return calcGacha(
+        if(p<0)return '????';  // 負の確率は NaN
+        if(p>1)return '????';  // 100%超の確率は NaN
+        if(N<1)return '????';  // 1回未満の試行では NaN
+        if(N<n)return '????';  // 試行回数以上の成功を求めれば NaN
+        if(n<0)return '????';  // 0回未満の成功を期待すれば NaN
+        var result = calcGacha(
           this.probabilityPerTry/100,
           this.numTries,
           this.numExpectedSuccesses
-        ) * 100;
+        );
+        return Number.isFinite(result) ? result*100 : '????';
       }
     }
   }
