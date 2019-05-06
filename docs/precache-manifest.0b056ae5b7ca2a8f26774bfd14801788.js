08b1fc138a519e619ac0e06c0f570d57ad7b4559
TkoolerLufar <TkoolerLufar@users.noreply.github.com>
2019-05-06 21:51:22 +0900

    本番ビルド

---
+++ docs/precache-manifest.0b056ae5b7ca2a8f26774bfd14801788.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://tkoolerlufar.github.io/probability-calc/robots.txt"
+  },
+  {
+    "revision": "9812f6b13b20d1b8a99b",
+    "url": "https://tkoolerlufar.github.io/probability-calc/js/chunk-vendors.c4230a26.js"
+  },
+  {
+    "revision": "73ec1431a5ac808b6fbf",
+    "url": "https://tkoolerlufar.github.io/probability-calc/js/app.922c8d91.js"
+  },
+  {
+    "revision": "d892c376dcaa357d7d2a9e1c711fca81",
+    "url": "https://tkoolerlufar.github.io/probability-calc/index.html"
+  },
+  {
+    "revision": "9812f6b13b20d1b8a99b",
+    "url": "https://tkoolerlufar.github.io/probability-calc/css/chunk-vendors.841ae584.css"
+  }
+];
---
+++ docs/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://tkoolerlufar.github.io/probability-calc/precache-manifest.0b056ae5b7ca2a8f26774bfd14801788.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "probability-calc"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
