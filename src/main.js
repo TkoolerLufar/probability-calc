a452d03617b63e4dfda17dc595f9c2d517c7a94c
TkoolerLufar <19635219+TkoolerLufar@users.noreply.github.com>
2025-08-02 19:23:09 +0000

    Vuetify 3 に余計なお世話となった互換機能を無効化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import {createApp} from 'vue'
+import {configureCompat, createApp} from 'vue'
 import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import './registerServiceWorker'
+
+configureCompat({
+    "COMPONENT_ASYNC": false,
+    "COMPONENT_V_MODEL": false
+})
 
 createApp(App).use(vuetify).mount('#app')
